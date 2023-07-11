10b71deb81f9dbecb89258c5b4309d6629fca480
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-07-11 16:21:51 +0300

    Check working

---
+++ QtDesigner Projects/TodoList/main.py
@@ -0,0 +1,26 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem
+from ui_TodoList import Ui_MainWindow
+from PySide6.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        todos = ["Finish writing the report", "Buy groceries", "Schedule a dentist appointment", "Pay bills", "Call John regarding the project", "Clean the garage", "Research vacation destinations", "Attend the team meeting at 2 PM", "Read the new book", "Start a workout routine"]
+
+        for todo in todos:
+            # self.ui.listWidget.addItem(todo)
+            item = QListWidgetItem(todo)
+            item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
+            item.setCheckState(Qt.Unchecked)
+            self.ui.todoList.addItem(item)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
