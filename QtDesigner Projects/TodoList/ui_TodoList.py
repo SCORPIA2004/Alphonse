9576a69a6a336d3534bd748a911c4f550d3ecf1a
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-07-12 16:22:36 +0300

    Hehe

--- QtDesigner Projects/TodoList/ui_TodoList.py
+++ QtDesigner Projects/TodoList/ui_TodoList.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'TodoListhDVnzf.ui'
+## Form generated from reading UI file 'TodoListYzrHtD.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.4.2
 ##
@@ -31,6 +31,7 @@ class Ui_MainWindow(object):
         self.todoList = QListWidget(self.centralwidget)
         self.todoList.setObjectName(u"todoList")
         self.todoList.setStyleSheet(u"font: 700 italic 12pt \"Segoe UI\";\n"
+"color: rgb(85, 170, 0);\n"
 "color: qradialgradient(spread:pad, cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 rgba(0, 0, 0, 0), stop:0.52 rgba(0, 0, 0, 0), stop:0.565 rgba(82, 121, 76, 33), stop:0.65 rgba(159, 235, 148, 64), stop:0.721925 rgba(255, 238, 150, 129), stop:0.77 rgba(255, 128, 128, 204), stop:0.89 rgba(191, 128, 255, 64), stop:1 rgba(0, 0, 0, 0));\n"
 "font: 20pt \"Ravie\";\n"
 "background-color: qradialgradient(spread:pad, cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 rgba(0, 0, 0, 255), stop:0.19397 rgba(0, 0, 0, 255), stop:0.202312 rgba(122, 97, 0, 255), stop:0.495514 rgba(76, 58, 0, 255), stop:0.504819 rgba(255, 255, 255, 255), stop:0.79 rgba(255, 255, 255, 255), stop:1 rgba(255, 158, 158, 255));")
