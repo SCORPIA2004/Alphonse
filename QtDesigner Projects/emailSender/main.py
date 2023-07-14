e43576983df35dbc1acda7146591c07b423aebc2
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-07-14 14:22:23 +0300

    Added Batch Email Functionality

--- QtDesigner Projects/emailSender/main.py
+++ QtDesigner Projects/emailSender/main.py
@@ -24,56 +24,71 @@ server.login(sender_email, app_password)
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        # QMainWindow.__init__(self)
-        self.ui = Ui_MainWindow()
-        # self.setWindowFlag(Qt.FramelessWindowHint)
 
-        # screenWidth = app.primaryScreen().size().width()
-        # self.setGeometry(screenWidth - 247, 0, 185, 45)
+        self.ui = Ui_MainWindow()
+
         self.ui.setupUi(self)
         self.receiverEmail = ""
         self.subject = ""
         self.body = ""
+        self.filename = ""
+        self.batch = False
 
         self.ui.pushButtonSend.clicked.connect(self.sendEmail)
         self.ui.pushButtonReset.clicked.connect(self.reset)
+
         self.ui.lineEditEmail.textChanged.connect(self.setReceiverEmail)
         self.ui.lineEditSubject.textChanged.connect(self.setSubject)
         self.ui.lineEditBody.textChanged.connect(self.setBody)
-        self.ui.pushButtonExit.clicked.connect(self.exitApp)
-        self.ui.pushButtonNew.clicked.connect(self.newEmail)
-        self.ui.stackedWidget.setCurrentIndex(0)
 
+        self.ui.pushButtonExit.clicked.connect(self.exitApp)
+        self.ui.pushButtonNew.clicked.connect(self.goToSingleOrBatchPage)
+
+        self.ui.pushButtonBatch.clicked.connect(self.goToUploadPage)
+        self.ui.pushButtonUpload.clicked.connect(self.uploadFile)
+        self.ui.pushButtonCompose.clicked.connect(self.newEmail)
+        self.ui.pushButtonSingle.clicked.connect(self.newEmail)
+
+        self.ui.stackedWidget.setCurrentIndex(0)
 
     def sendEmail(self):
-        message = MIMEMultipart()
+        if self.batch:
+            print("Sending batch")
+            self.batchSend()
+        else:
+            message = MIMEMultipart()
 
-        # Add the body of the email to the MIME message
 
-        # message.attach(self.body)
-        body = MIMEText(self.body)
-        message.attach(body)
+            body = MIMEText(self.body)
+            message.attach(body)
 
-        # Add the attachment to the MIME message
-        with open("Muhammad Shayan Resume.pdf", "rb") as f:
-            attach = MIMEApplication(f.read(), _subtype="pdf")
-            attach.add_header("Content-Disposition", "attachment", filename="Muhammad Shayan Usman Resume.pdf")
-            message.attach(attach)
+            with open("Muhammad Shayan Resume.pdf", "rb") as f:
+                attach = MIMEApplication(f.read(), _subtype="pdf")
+                attach.add_header("Content-Disposition", "attachment", filename="Muhammad Shayan Usman Resume.pdf")
+                message.attach(attach)
 
-        message["Subject"] = self.subject
-        message["From"] = sender_email
-        message["To"] = self.receiverEmail
+            message["Subject"] = self.subject
+            message["From"] = sender_email
+            message["To"] = self.receiverEmail
 
-        receipientEmails = [self.receiverEmail]
+            receipientEmails = [self.receiverEmail]
 
-        server.sendmail(sender_email, receipientEmails, message.as_string())
-        print(f"Sent email to {self.receiverEmail}")
+            server.sendmail(sender_email, receipientEmails, message.as_string())
+            print(f"Sent email to {self.receiverEmail}")
+            self.ui.stackedWidget.setCurrentIndex(3)
+            self.ui.labelEmailSent.setText(f"Sent single email to {self.receiverEmail}")
+
+    def goToUploadPage(self):
+        self.batch = True
         self.ui.stackedWidget.setCurrentIndex(1)
-        self.ui.label.setText(f"Sent email to {self.receiverEmail}")
 
-    def newEmail(self):
+    def goToSingleOrBatchPage(self):
         self.ui.stackedWidget.setCurrentIndex(0)
+    def newEmail(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
         self.reset()
+
+
 
     def setReceiverEmail(self, str):
         self.receiverEmail = str
@@ -88,12 +103,55 @@ class MainWindow(QMainWindow):
         self.ui.lineEditEmail.setText("")
         self.ui.lineEditSubject.setText("")
         self.ui.lineEditBody.setText("")
+        self.receiverEmail = ""
+        self.subject = ""
+        self.body = ""
 
     def exitApp(self):
         server.quit()
         QCoreApplication.instance().quit()
 
+    def uploadFile(self):
+        filenameArr, selectedFilter = QFileDialog.getOpenFileNames(self)
+        self.filename = filenameArr[0]
+        self.ui.labelFileName.setText(self.filename)
 
+
+    def batchSend(self):
+        recipients = []
+
+        with open(self.filename, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                recipients.append(row[0])
+
+        count = 0
+
+        for recipient in recipients:
+            count += 1
+            message = MIMEMultipart()
+
+            body = MIMEText(self.body)
+            message.attach(body)
+
+            # Add the attachment to the MIME message
+            with open("Muhammad Shayan Resume.pdf", "rb") as f:
+                attach = MIMEApplication(f.read(), _subtype="pdf")
+                attach.add_header("Content-Disposition", "attachment", filename="Muhammad Shayan Usman Resume.pdf")
+                message.attach(attach)
+
+            message["Subject"] = self.subject
+            message["From"] = sender_email
+            message["To"] = recipient
+
+            # receipientEmails = [self.receiverEmail]
+
+            server.sendmail(sender_email, recipient, message.as_string())
+            print(f"Sent email to {recipient}")
+
+        self.ui.labelEmailSent.setText(f"Sent email to {count} recipients")
+        self.ui.stackedWidget.setCurrentIndex(3)
+        self.batch = False
 
 
 app = QApplication(sys.argv)
--- QtDesigner Projects/emailSender/ui_main.py
+++ QtDesigner Projects/emailSender/ui_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'mainKVGOes.ui'
+## Form generated from reading UI file 'mainlcxgIF.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.4.2
 ##
@@ -72,42 +72,81 @@ class Ui_MainWindow(object):
         self.gridLayout.setObjectName(u"gridLayout")
         self.stackedWidget = QStackedWidget(self.centralwidget)
         self.stackedWidget.setObjectName(u"stackedWidget")
-        self.page = QWidget()
-        self.page.setObjectName(u"page")
-        self.lineEditBody = QLineEdit(self.page)
+        self.pageSingleOrBatch = QWidget()
+        self.pageSingleOrBatch.setObjectName(u"pageSingleOrBatch")
+        self.gridLayout_3 = QGridLayout(self.pageSingleOrBatch)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.pushButtonSingle = QPushButton(self.pageSingleOrBatch)
+        self.pushButtonSingle.setObjectName(u"pushButtonSingle")
+
+        self.gridLayout_3.addWidget(self.pushButtonSingle, 0, 0, 1, 1)
+
+        self.pushButtonBatch = QPushButton(self.pageSingleOrBatch)
+        self.pushButtonBatch.setObjectName(u"pushButtonBatch")
+        self.pushButtonBatch.setMaximumSize(QSize(200, 16777215))
+
+        self.gridLayout_3.addWidget(self.pushButtonBatch, 1, 0, 1, 1)
+
+        self.stackedWidget.addWidget(self.pageSingleOrBatch)
+        self.pageUploadFile = QWidget()
+        self.pageUploadFile.setObjectName(u"pageUploadFile")
+        self.gridLayout_4 = QGridLayout(self.pageUploadFile)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.pushButtonUpload = QPushButton(self.pageUploadFile)
+        self.pushButtonUpload.setObjectName(u"pushButtonUpload")
+        self.pushButtonUpload.setMaximumSize(QSize(200, 16777215))
+
+        self.gridLayout_4.addWidget(self.pushButtonUpload, 0, 0, 1, 1)
+
+        self.labelFileName = QLabel(self.pageUploadFile)
+        self.labelFileName.setObjectName(u"labelFileName")
+        self.labelFileName.setMaximumSize(QSize(300, 20))
+
+        self.gridLayout_4.addWidget(self.labelFileName, 0, 1, 1, 1)
+
+        self.pushButtonCompose = QPushButton(self.pageUploadFile)
+        self.pushButtonCompose.setObjectName(u"pushButtonCompose")
+        self.pushButtonCompose.setMaximumSize(QSize(200, 16777215))
+
+        self.gridLayout_4.addWidget(self.pushButtonCompose, 1, 0, 1, 1)
+
+        self.stackedWidget.addWidget(self.pageUploadFile)
+        self.pageComposeSingle = QWidget()
+        self.pageComposeSingle.setObjectName(u"pageComposeSingle")
+        self.lineEditBody = QLineEdit(self.pageComposeSingle)
         self.lineEditBody.setObjectName(u"lineEditBody")
         self.lineEditBody.setGeometry(QRect(-5, 94, 483, 170))
         self.lineEditBody.setMinimumSize(QSize(0, 170))
         self.lineEditBody.setReadOnly(False)
-        self.pushButtonSend = QPushButton(self.page)
+        self.pushButtonSend = QPushButton(self.pageComposeSingle)
         self.pushButtonSend.setObjectName(u"pushButtonSend")
         self.pushButtonSend.setGeometry(QRect(-5, 270, 239, 36))
         self.pushButtonSend.setMinimumSize(QSize(50, 0))
         self.pushButtonSend.setMaximumSize(QSize(16777215, 40))
-        self.lineEditEmail = QLineEdit(self.page)
+        self.lineEditEmail = QLineEdit(self.pageComposeSingle)
         self.lineEditEmail.setObjectName(u"lineEditEmail")
         self.lineEditEmail.setGeometry(QRect(-5, 2, 483, 40))
         self.lineEditEmail.setMinimumSize(QSize(0, 30))
-        self.lineEditSubject = QLineEdit(self.page)
+        self.lineEditSubject = QLineEdit(self.pageComposeSingle)
         self.lineEditSubject.setObjectName(u"lineEditSubject")
         self.lineEditSubject.setGeometry(QRect(-5, 48, 483, 40))
         self.lineEditSubject.setMinimumSize(QSize(0, 30))
-        self.pushButtonReset = QPushButton(self.page)
+        self.pushButtonReset = QPushButton(self.pageComposeSingle)
         self.pushButtonReset.setObjectName(u"pushButtonReset")
         self.pushButtonReset.setGeometry(QRect(240, 270, 238, 36))
         self.pushButtonReset.setMaximumSize(QSize(16777215, 40))
-        self.stackedWidget.addWidget(self.page)
-        self.page_3 = QWidget()
-        self.page_3.setObjectName(u"page_3")
-        self.gridLayout_2 = QGridLayout(self.page_3)
+        self.stackedWidget.addWidget(self.pageComposeSingle)
+        self.pageSuccessfullySent = QWidget()
+        self.pageSuccessfullySent.setObjectName(u"pageSuccessfullySent")
+        self.gridLayout_2 = QGridLayout(self.pageSuccessfullySent)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.label = QLabel(self.page_3)
-        self.label.setObjectName(u"label")
-        self.label.setMaximumSize(QSize(16777215, 40))
+        self.labelEmailSent = QLabel(self.pageSuccessfullySent)
+        self.labelEmailSent.setObjectName(u"labelEmailSent")
+        self.labelEmailSent.setMaximumSize(QSize(16777215, 40))
 
-        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 2)
+        self.gridLayout_2.addWidget(self.labelEmailSent, 0, 0, 1, 2)
 
-        self.pushButtonExit = QPushButton(self.page_3)
+        self.pushButtonExit = QPushButton(self.pageSuccessfullySent)
         self.pushButtonExit.setObjectName(u"pushButtonExit")
         sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
@@ -118,7 +157,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.pushButtonExit, 1, 0, 1, 1)
 
-        self.pushButtonNew = QPushButton(self.page_3)
+        self.pushButtonNew = QPushButton(self.pageSuccessfullySent)
         self.pushButtonNew.setObjectName(u"pushButtonNew")
         sizePolicy1.setHeightForWidth(self.pushButtonNew.sizePolicy().hasHeightForWidth())
         self.pushButtonNew.setSizePolicy(sizePolicy1)
@@ -126,7 +165,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.pushButtonNew, 1, 1, 1, 1)
 
-        self.stackedWidget.addWidget(self.page_3)
+        self.stackedWidget.addWidget(self.pageSuccessfullySent)
 
         self.gridLayout.addWidget(self.stackedWidget, 0, 0, 1, 1)
 
@@ -134,7 +173,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(0)
+        self.stackedWidget.setCurrentIndex(3)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -142,12 +181,17 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Email Sender", None))
+        self.pushButtonSingle.setText(QCoreApplication.translate("MainWindow", u"Single", None))
+        self.pushButtonBatch.setText(QCoreApplication.translate("MainWindow", u"Batch", None))
+        self.pushButtonUpload.setText(QCoreApplication.translate("MainWindow", u"Upload .csv file", None))
+        self.labelFileName.setText(QCoreApplication.translate("MainWindow", u"No file selected", None))
+        self.pushButtonCompose.setText(QCoreApplication.translate("MainWindow", u"Compose", None))
         self.lineEditBody.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Body", None))
         self.pushButtonSend.setText(QCoreApplication.translate("MainWindow", u"Send", None))
         self.lineEditEmail.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Email", None))
         self.lineEditSubject.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Subject", None))
         self.pushButtonReset.setText(QCoreApplication.translate("MainWindow", u"Reset", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Email sent", None))
+        self.labelEmailSent.setText(QCoreApplication.translate("MainWindow", u"Email sent", None))
         self.pushButtonExit.setText(QCoreApplication.translate("MainWindow", u"Close", None))
         self.pushButtonNew.setText(QCoreApplication.translate("MainWindow", u"New", None))
     # retranslateUi
