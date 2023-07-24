559d19e760dfdab82fd22599912337738bce82f3
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-07-24 13:40:24 +0300

    Leaflet

--- Inderse/inderse/inderse.ino
+++ Inderse/inderse/inderse.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ArduinoHttpClient.h>
+#include <ESP8266WiFi.h>
+#include <HTTPClient.h>
 
+
+#define ssid "MetroBandAP"
+#define password "SyQGaMq6ahrE"
+
+void setup()
+{
+  // setup
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+  
+  Serial.begin(11520);
+
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    
+  }
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  // 
 }
