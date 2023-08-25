51fc9d4e28dabd6ef42c8ae0e675e2f56d40c6f5
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-08-25 14:52:37 +0300

    Fixed Area enclosed

--- VSM Analysis/saturationField.py
+++ VSM Analysis/saturationField.py
@@ -23,7 +23,17 @@ def computeArea(pos):
     x, y = (zip(*pos))
     return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
 
-def areaEnclosed(x, y, polygon):
+def areaEnclosed(x, y):
+    # Plot the results
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, y, label='Hysteresis', linewidth=2)
+    # plot a vertical line at the knee
+    if saturationPoint != 0:
+        plt.vlines(saturationPoint, plt.ylim()[0], plt.ylim()[1], linestyles='dashed', colors='r', label='Saturation point')
+
+    # index = np.where(x == x.max())[0].item()
+    # polygon = plt.fill(x[index:], y[index:], 'b', alpha=0.3)
+    polygon = plt.fill(x, y, 'b', alpha=0.3)
     area = computeArea(polygon[0].xy)
     print("Area: ", area)
     return area
