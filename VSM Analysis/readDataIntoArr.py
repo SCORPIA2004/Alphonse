77bdbad4883f1b14291beb7d1a1415e83971feef
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-08-28 11:11:35 +0300

    Fixed columns

--- VSM Analysis/readDataIntoArr.py
+++ VSM Analysis/readDataIntoArr.py
@@ -15,38 +15,44 @@ def readDataIntoArr():
     print(filename)
 
     if filename.endswith('.csv'):
-        with open(filename, "r", newline="") as f:
-            reader = csv.reader(f)
-            for row in reader:
-                try:
-                    row = reader.__next__()
-                except StopIteration:
-                    break
-                column1.append(row[0])
-                column2.append(row[1])
-                try:
-                    column3.append(row[2])
-                except IndexError:
-                    pass
+        # Replace 'your_file.csv' with the actual file path
+        column_arrays = []
+
+        with open(filename, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+
+            # Read the first row to get column names
+            columns = next(csv_reader)
+
+            # Initialize arrays for each column
+            for _ in columns:
+                column_arrays.append([])
+
+            # Read and store data into arrays
+            for row in csv_reader:
+                for i, value in enumerate(row):
+                    column_arrays[i].append(value)
+
     elif filename.endswith('.xlsx'):
         # Replace 'your_file.xlsx' with the actual file path
         data = pd.read_excel(filename)
 
         # Separate columns into arrays
         column_arrays = [data[column_name].tolist() for column_name in data.columns]
+        # # Now each column's data is stored in a separate array in 'column_arrays'
+        # column1 = column_arrays[0]
+        # column2 = column_arrays[1]
+        # column3 = column_arrays[2]
 
-        # Now each column's data is stored in a separate array in 'column_arrays'
-        column1 = column_arrays[0]
-        column2 = column_arrays[1]
-        column3 = column_arrays[2]
-
-    column1 = np.array(column1)
-    column2 = np.array(column2)
-    column3 = np.array(column3)
-    return column1, column2, column3
-
+    # column1 = np.array(column1)
+    # column2 = np.array(column2)
+    # column3 = np.array(column3)
+    # return column1, column2, column3
+    return column_arrays
 if __name__ == '__main__':
-    c1, c2, c3 = readDataIntoArr()
-    print(c1)
-    print(c2)
-    print(c3)
+    columns = readDataIntoArr()
+    print(columns)
+    # c1, c2, c3 = readDataIntoArr()
+    # print(c1)
+    # print(c2)
+    # print(c3)
